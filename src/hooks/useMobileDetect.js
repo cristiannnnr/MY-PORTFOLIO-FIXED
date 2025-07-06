6930a91296b8e5409f6cfbf069bf121094374174
cristiannnnr <151454026+cristiannnnr@users.noreply.github.com>
2025-07-06 15:05:17 -0500

    Add files via upload

--- src/hooks/useMobileDetect.js
+++ src/hooks/useMobileDetect.js
@@ -1,1 +1,20 @@
+import { useEffect, useState } from 'react';
 
+export default function useMobileDetect() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkIfMobile = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    checkIfMobile();
+    window.addEventListener('resize', checkIfMobile);
+    
+    return () => {
+      window.removeEventListener('resize', checkIfMobile);
+    };
+  }, []);
+
+  return isMobile;
+}
